1b9a9877baf5082ac05497fd284d8e17aa86e027
PulseTrade5 <prabhat3300@gmail.com>
2026-06-21 20:36:49 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>PulseTrade 🐂</h1>
+      <p>NSE/BSE Technical Analysis Platform</p>
+      <p>Coming Soon...</p>
+    </div>
+  )
+}
