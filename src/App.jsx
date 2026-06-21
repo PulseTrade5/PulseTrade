a341ddd06faead186ff19a93773145d3e62538aa
PulseTrade5 <prabhat3300@gmail.com>
2026-06-22 00:03:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import SubscribeButton from './SubscribeButton';
+
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
       <h1>PulseTrade 🐂</h1>
       <p>NSE/BSE Technical Analysis Platform</p>
       <p>Coming Soon...</p>
+      <SubscribeButton userEmail="" userName="" />
     </div>
   )
 }
