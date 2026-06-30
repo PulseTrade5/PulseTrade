7a7e31ddba02b5aacfdf7063e52a22398d9ec4b1
PulseTrade5 <prabhat3300@gmail.com>
2026-06-30 23:41:43 +0530

    Update Supabase service role key variable name

--- api/fii-dii.js
+++ api/fii-dii.js
@@ -8,7 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // service role needed to write
+  process.env.SUPABASE_SERVICE_KEY // service role needed to write
 );
 
 const NSE_BASE = "https://www.nseindia.com";
